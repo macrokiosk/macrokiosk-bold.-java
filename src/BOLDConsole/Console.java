48f4a7d3a996c55737a7fb60c3d4c29728fe1d9f
KUM HUN KIT <khkit@hotmail.com>
2020-05-28 17:17:59 +0800

    new commit

---
+++ src/BOLDConsole/Console.java
@@ -0,0 +1,72 @@
+package BOLDConsole;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Console {
+     private final HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .build();
+    
+    public static void main(String[] args) throws Exception {
+                
+        Console obj = new Console();
+        
+        //SENDING SMS VIA REST HTTP GET
+        System.out.println("Send HTTP GET request");
+        obj.sendGet();
+
+        //SENDING SMS VIA SOAP WEB SERVICE
+        System.out.println("Send HTTP POST request");
+        obj.sendPost();
+                
+    }
+    
+    private void sendGet() throws Exception {
+        String requestUri1 = String.format("http://www.etracker.cc/bulksms/send?user=%1$s&pass=%2$s&type=%3$s&to=%4$s&from=%5$s&text=%6$s&servid=%7$s",
+                    "username",
+                    "password",
+                    "0",
+                    "60123456789",
+                    "from",
+                    "test",
+                    "serviceid");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(requestUri1))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
+    }
+
+    private void sendPost() throws Exception {
+
+        //Build the request json
+        String json = "{\"user\":\"username\"," +
+            "\"pass\":\"password\"," +
+            "\"type\":\"0\"," +
+            "\"to\":\"60123456789\"," +
+            "\"from\":\"from\"," +
+            "\"text\":\"test\"," +
+            "\"servid\":\"serviceid\"}";
+        
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .uri(URI.create("http://www.etracker.cc/bulksms/send"))
+                .header("Content-Type", "application/json")
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
+    }    
+}
